b2f54c78a13865c3716810dac78b8527e5d7f432
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2026-01-09 11:10:43 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let strip = neopixel.create(DigitalPin.P1, 30, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Red))
 strip.setPixelColor(26, neopixel.colors(NeoPixelColors.Indigo))
 strip.setPixelColor(27, neopixel.colors(NeoPixelColors.Indigo))
 strip.setPixelColor(28, neopixel.colors(NeoPixelColors.Indigo))
@@ -6,7 +7,6 @@ strip.show()
 for (let index = 0; index < 4; index++) {
     basic.pause(2000)
     strip.rotate(1)
-    strip.show()
 }
 basic.forever(function () {
 	
